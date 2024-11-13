9f7a250a4f7d98e1a2f348965a18ac1661fa2dfe
ThePixelMoon <yourlocalmoon7@gmail.com>
2024-11-13 12:12:20 +0200

    add

--- build.cmd
+++ build.cmd
@@ -1,8 +1,12 @@
 @echo off
-if not exist build (
-    mkdir build
-)
+mkdir build
 cd build
+rem we use cmake!!
 cmake ..
 cmake --build .
+rem mmm... copy..
+xcopy /Y /E /I "..\res" "Debug"
+cd Debug
+alter-engine.exe
+cd ..
 cd ..
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,58 @@
-#include <iostream>
+// main.cpp
+#define SDL_MAIN_HANDLED
+
+#include <SDL2/SDL.h>
 
 int main()
 {
-    
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        SDL_Log("SDL_Init Error: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Alter Engine", 
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+                                          800, 600, SDL_WINDOW_SHOWN);
+
+    if (window == nullptr)
+    {
+        SDL_Log("SDL_CreateWindow Error: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        SDL_Log("SDL_CreateRenderer Error: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool isRunning = true;
+
+    SDL_Event e;
+    while (isRunning)
+    {
+        while (SDL_PollEvent(&e) != 0)
+        {
+            if (e.type == SDL_QUIT)
+            {
+                isRunning = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
